a452303d71435bf21beb8d2968351cb98999a465
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-09-20 13:18:49 +0100

    fix

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,5 @@
-const round = Math.round;
-
 function calculateNumber(a, b) {
-  return round(a) + round(b);
+  return Math.round(a) + Math.round(b);
 }
 
-module.exports = calculateNumber
+module.exports = calculateNumber;
