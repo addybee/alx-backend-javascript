743c0cfd723c078abc60f8b78c6cbfb3a9f03d4e
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-09-22 15:03:29 +0100

    fix

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,21 +1,19 @@
 import readDatabase from '../utils';
 
-const dbFilename = process.argv[2] || 'database.csv';
-
-
 class StudentsController {
   static getAllStudents(request, response) {
     response.statusCode = 200;
-    readDatabase(dbFilename)
+    readDatabase(process.argv[2] || 'database.csv')
       .then((data) => {
         let message = 'This is the list of our students';
         const fields = Object.keys(data).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }));
         fields.forEach((field) => {
           message += `\nNumber of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}`;
         });
-        response.send(message)
+        response.send(message);
       })
       .catch((err) => {
+        console.error(err);
         response.status(500).send(err.message);
       });
   }
@@ -26,12 +24,12 @@ class StudentsController {
     if (!['CS', 'SWE'].includes(major)) {
       response.status(500).send('Major parameter must be CS or SWE');
     } else {
-
-      readDatabase(dbFilename)
+      readDatabase(process.argv[2] || 'database.csv')
         .then((data) => {
           response.send(`List: ${data[major].join(', ')}`);
         })
         .catch((err) => {
+          console.error(err);
           response.status(500).send(err.message);
         });
     }
