aa9de9e65ef54831ffb0429a6e1080f5ab9ba7b9
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-09-22 04:26:56 +0100

    fix

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -8,16 +8,15 @@ class StudentsController {
     response.statusCode = 200;
     readDatabase(dbFilename)
       .then((data) => {
-        response.write('This is the list of our students');
+        let message = 'This is the list of our students';
         const fields = Object.keys(data).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }));
         fields.forEach((field) => {
-          response.write(`\nNumber of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}`);
+          message += `\nNumber of students in ${field}: ${data[field].length}. List: ${data[field].join(', ')}`;
         });
-        response.send()
+        response.send(message)
       })
       .catch((err) => {
-        response.statusCode = 500;
-        response.send('Cannot load the database');
+        response.status(500).send(err.message);
       });
   }
 
@@ -25,18 +24,17 @@ class StudentsController {
     response.statusCode = 200;
     const { major } = request.params;
     if (!['CS', 'SWE'].includes(major)) {
-      response.statusCode = 500;
-      response.send('Major parameter must be CS or SWE');
-    }
+      response.status(500).send('Major parameter must be CS or SWE');
+    } else {
 
-    readDatabase(dbFilename)
-      .then((data) => {
-        response.send(`List: ${data[major].join(', ')}`);
-      })
-      .catch((err) => {
-        response.statusCode = 500;
-        response.send('Cannot load the database');
-      });
+      readDatabase(dbFilename)
+        .then((data) => {
+          response.send(`List: ${data[major].join(', ')}`);
+        })
+        .catch((err) => {
+          response.status(500).send(err.message);
+        });
+    }
   }
 }
 
