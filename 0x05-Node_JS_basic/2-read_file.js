22de758802e5a5dccaa8b8d4509ba4fd9d225351
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-09-22 15:51:22 +0100

    fix task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,19 +1,26 @@
-const { execSync } = require('child_process');
+const { readFileSync } = require('fs');
 
 function countStudents(path) {
   try {
-    const fileContent = execSync(`cat "${path}"`, { encoding: 'utf-8' }).trim(); // Wrap path in quotes to prevent injection
-    const lineCount = execSync(`echo "${fileContent}" | wc -l`, { encoding: 'utf-8' }).trim(); // Count lines in the file content
-    console.log('Number of students:', lineCount - 1);
-    // Extract field names from the filecontent and remove the header
-    const fields = execSync(`echo "${fileContent}" | awk -F, 'NR>1 {print $4}' | sort | uniq`, { encoding: 'utf-8' }).trim().split('\n');
-    fields.forEach((field) => {
-      const counts = execSync(`echo "${fileContent}" | awk -F, -v field="${field}" '$4 == field {count++} END {print count}'`, { encoding: 'utf-8' }).trim();
-      const names = execSync(`echo "${fileContent}" | awk -F, -v field="${field}" '$4 == field {print $1}'`, { encoding: 'utf-8' }).trim().split('\n').join(', ');
-      console.log(`Number of students in ${field}: ${counts}. List: ${names}`);
+    const data = readFileSync(path, { encoding: 'utf-8' });
+
+    const lines = data.split('\n').slice(1);
+    const fieldObject = {};
+
+    lines.forEach((val) => {
+      const [firstname, , , field] = val.trim().split(',');
+      if (!fieldObject[field]) {
+        fieldObject[field] = [];
+      }
+      fieldObject[field].push(firstname);
     });
-  } catch (err) {
+    console.log(`Number of students: ${lines.length}`);
+    Object.keys(fieldObject).forEach((key) => {
+      console.log(`Number of students in ${key}: ${fieldObject[key].length}. List: ${fieldObject[key].join(', ')}`);
+    });
+  } catch (error) {
     throw new Error('Cannot load the database');
   }
 }
+
 module.exports = countStudents;
